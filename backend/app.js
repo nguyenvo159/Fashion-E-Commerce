e1cc98446d502ed63725077335d7f611ec668eea
TUF <nguyenvo1373.hg@gmail.com>
2024-01-17 16:34:17 +0700

    Backend -- connect, install

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const product = require("./app/routes/product.route");
+const user = require("./app/routes/user.route");
+const cart = require("./app/routes/cart.route");
+const order = require("./app/routes/order.route");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Backend" });
+});
+
+// app.use("/api/product", product);
+// app.use("/api/auth", user);
+// app.use("/api/cart", cart);
+// app.use("api/order", order);
+
+module.exports = app;
