a286bf138684c484d260b6d1eaa8538e0e3e9563
TUF <nguyenvo1373.hg@gmail.com>
2024-03-11 01:48:55 +0700

    BE - Analtics

--- backend/app/routes/analytics.route.js
+++ backend/app/routes/analytics.route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const analytics = require("../controllers/analytics.controller");
 
-router.post("/revenue-by-day", analytics.getRevenueByDay);
-router.post("/product-category", analytics.getProductCategory);
+router.post("/revenue-month", analytics.getRevenueByDay);
+router.post("/product-ratio", analytics.getProductCategory);
 
 module.exports = router;
+
+
