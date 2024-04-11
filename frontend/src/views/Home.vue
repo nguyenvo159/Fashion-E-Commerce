ff68c1f68f27d64c565891c1f7411e3dc51c4452
TUF <nguyenvo1373.hg@gmail.com>
2024-04-11 20:50:53 +0700

    FE - Fix Home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container-fluid row justify-content-center m-0 p-0">
-        <div id="carouselExampleIndicators" class="col-11 carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
